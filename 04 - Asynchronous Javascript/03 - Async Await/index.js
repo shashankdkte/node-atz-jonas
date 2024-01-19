063a9e1105825858d7cf15fecbbe58973aff89c1
Shashank Dhakate <shashankdkte@gmail.com>
2024-01-19 17:50:57 +0530

    Async await

---
+++ 04 - Asynchronous Javascript/03 - Async Await/index.js
@@ -0,0 +1,43 @@
+const fs = require("fs");
+const superagent = require("superagent");
+
+const readFilePro = file => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(file, (err, data) => {
+      if(err) reject('Unable to find the file 😅')
+      resolve(data);
+    })
+  })
+}
+
+const writeFilePro = (file, data) => {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(file, data, err => {
+      if (err) reject("Unable to Write to File");
+      resolve('success');
+    })
+  })
+  
+} 
+
+
+const getDogPic = async () => {
+  try {
+      const data = await readFilePro(`${__dirname}/dog.txt`);
+  console.log(`Breed: ${data}`);
+  
+  const res = await superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
+  console.log(res.body.message);
+
+
+  await writeFilePro('dog-img.txt', res.body.message);
+  console.log(`Random dog image saved to file`)
+  } catch (error) {
+    console.log(error)
+  }
+
+  
+
+}
+
+getDogPic();
