17069b55565428b7a5d5b9409142c33b8d87747c
Shashank Dhakate <shashankdkte@gmail.com>
2024-01-19 17:31:10 +0530

    Promises Refactored

--- 04 - Asynchronous Javascript/01 - Callback Hell/code/index.js
+++ 04 - Asynchronous Javascript/01 - Callback Hell/code/index.js
@@ -1,62 +1,16 @@
 const fs = require("fs");
 const superagent = require("superagent");
 
-const readFilePro = file => {
-  return new Promise((resolve, reject) => {
-    fs.readFile(file, (err, data) => {
-      if(err) reject('Unable to find the file 😅')
-      resolve(data);
-    })
-  })
-}
-
-const writeFilePro = (file, data) => {
-  return new Promise((resolve, reject) => {
-    fs.writeFile(file, data, err => {
-      if (err) reject("Unable to Write to File");
-      resolve('success');
-    })
-  })
-  
-} 
-
-readFilePro(`${__dirname}/dog.txt`)
-  .then(data => {
-    console.log(`Breed: ${data}`);
-    return superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
-  })
-   .then(res =>{
-      console.log(res.body.message);
-     return writeFilePro("dog-img.txt", res.body.message);
-
-    // fs.writeFile("dog-img.txt", res.body.message, err => {
-    //   console.log('Random dog image saved to file')
-    // })
-   })
-  .then(() => {
-    console.log('Random dog image saved to file')
-  })
-  .catch((err) => {
+fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
+  console.log(`Breed: ${data}`);
+  superagent.get(`https://dog.ceo/api/breed/${data}/images/random`)
+    .end((err, res) => {
     
-    console.log(err.message);
+      if (err) return console.log(err.message);
+    console.log(res.body.message);
     
+    fs.writeFile("dog-img.txt", res.body.message, err => {
+      console.log('Random dog image saved to file')
+    })
 })
-
-
-// fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
-//   console.log(`Breed: ${data}`);
-//   superagent.get(`https://dog.ceo/api/breed/${data}/images/random`)
-//     .then(res =>{
-//       console.log(res.body.message);
-    
-//     fs.writeFile("dog-img.txt", res.body.message, err => {
-//       console.log('Random dog image saved to file')
-//     })
-//     })
-//     .catch((err) => {
-    
-//     console.log(err.message);
-    
-// })
-// })
-
+})
